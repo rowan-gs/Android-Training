ce22ea7d49cf1749bc0af7d3acc85ddc1be17ac1
Rowan Walker <rowan.walker@gridstone.com.au>
2017-08-03 14:11:29 +1000

    Cleanup RecyclerView item layout, add dummy values

--- app-kotlin/src/main/kotlin/au/com/gridstone/training_kotlin/model/Model.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/training_kotlin/model/Model.kt
@@ -7,6 +7,11 @@ class Model {
     var posts: Array<ImgurPost>
 
     init {
-        posts = arrayOf(ImgurPost(), ImgurPost(), ImgurPost(), ImgurPost(), ImgurPost())
+        posts = arrayOf(
+                ImgurPost("This cat is pretty good"),
+                ImgurPost("I like this cat too"),
+                ImgurPost("Cats are pretty crazy!"),
+                ImgurPost("Despite everything, it's a cat"),
+                ImgurPost("A fifth cat for good measure"))
     }
 }
